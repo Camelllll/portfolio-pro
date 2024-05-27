d1d53760db4c33fd9382d69b0a915527b7f27719
Camélien <camelien.tournu@etudiant.univ-reims.fr>
2024-05-27 14:08:24 +0200

    Feature: create first page with navbar

--- Portfolio-pro/src/App.vue
+++ Portfolio-pro/src/App.vue
@@ -1,39 +1,131 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import AboutView from '@/views/AboutView.vue';
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <RouterLink to="/">
-        <img src="@/assets/img/camel.png" alt="logo" class="logo" />
-      </RouterLink>
-      <nav class="navbar">
-        <RouterLink to="/">Accueil</RouterLink>
-        <RouterLink to="/about">À Propos</RouterLink>
-        <RouterLink to="/projects">Projets</RouterLink>
-        <RouterLink to="/contact">Contact</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <main class="centered">
-    <h1>Développeur Web Full Stack</h1>
-    <h2>Passionné par le développement web et les nouvelles technologies</h2>
-  </main>
+  <div>
+    <section class="section home">
+      <div class="content">
+        <header>
+          <div class="wrapper">
+            <nav class="navbar">
+              <RouterLink to="/" class="title-link">
+                <h1 class="logo_title"> Camélien</h1>
+                <h2 class="subtitle_title">Développeur Web</h2>
+              </RouterLink>
+
+              <RouterLink to="/about">À Propos</RouterLink>
+              <RouterLink to="/projects">Projets</RouterLink>
+                <RouterLink to="/contact">
+                  <button class="btn">
+                    Contact
+                  </button>
+                </RouterLink>
+            </nav>
+          </div>
+        </header>
+        <main class="centered">
+          <h1>Développeur Web Full Stack</h1>
+          <h2>Passionné par le<span class="color-span">développement web</span>  et les nouvelles technologies</h2>
+          <div class="center-img">
+            <img src="@/assets/img/mf-avatar.svg" alt="developer" />
+          </div>
+        </main>
+      </div>
+    </section>
+
+    <section id="about" class="section">
+      <AboutView />
+    </section>
+
+    <section id="projects" class="section">
+      <ProjectView />
+    </section>
+
+  </div>
 </template>
 
 <style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=REM:ital,wght@0,100..900;1,100..900&display=swap');
+@import url('https://fonts.cdnfonts.com/css/gilroy-bold');
+
+
+* {
+  font-family: 'Gilroy-Bold', sans-serif;
+  font-weight: 400;
+}
+
+.background {
+  position: relative;
+  height: 100vh;
+}
+
+.color-span {
+  color: #7510f6;
+}
+
+.section {
+  height: 100vh;
+  position: relative;
+  background-color: #fff;
+}
+
+.content {
+  position: relative;
+  z-index: 1;
+}
+
+.btn {
+  padding: 10px 20px;
+  background-color: transparent;
+  border: 2px solid #7510f6;
+  color: #7510f6;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  font-family: 'Gilroy-Bold', sans-serif;
+  font-weight: 400;
+  font-size: 14px;
+
+  &:hover {
+    background-color: #7510f6;
+    color: white;
+  }
+}
+
 .wrapper {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-top: -50px;
 }
 
-.logo {
-  height: 150px;
-  margin-right: 800px;
-  margin-left: -40px;
+.title-link {
+  display: flex;
+  flex-direction: column;
+  color: #141c3a;
+  text-decoration: none;
+  margin-right: 790px;
+  margin-left: 80px;
+
+  .logo_title {
+    font-size: 30px;
+    font-weight: 700;
+    line-height: 1.2;
+    font-family: 'Gilroy-Bold', sans-serif;
+    font-weight: 400;
+  }
+
+  .subtitle_title {
+    font-size: 12px;
+    font-weight: 600;
+    line-height: 0.3;
+    font-family: 'Gilroy-Bold', sans-serif;
+    font-weight: 400;
+    margin-left: 14px;
+    color: #7510f6;
+  }
 }
 
 .centered {
@@ -41,25 +133,54 @@ import { RouterLink, RouterView } from 'vue-router'
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 60vh;
+  height: 89vh;
   text-align: center;
+  color: #141c3a;
+  font-family: 'Gilroy-Medium', sans-serif;
+
+  h1 {
+    font-size: 44px;
+    font-weight: 700;
+  }
+
+  h2 {
+    font-size: 24px;
+    font-weight: 400;
+  }
+}
+
+.center-img {
+  margin-top: 20px;
+  img {
+    width: 230px;
+    height: 230px;
+    border-radius: 50%;
+  }
 }
 
 .navbar {
   display: flex;
-  gap: 40px; 
-  margin-left: 90px;
+  align-items: center;
+  gap: 30px;
+  margin-top: 20px;
+  
 
   a {
     text-decoration: none;
-    color: #000;
+    color: #141c3a;
     font-size: 16px;
+    font-family: 'Gilroy-Bold', sans-serif;
     font-weight: 500;
     transition: all 0.3s ease;
 
     &:hover {
-      color: #f00;
+      color: #7510f6;
     }
   }
+}
+
+#about {
+  background-color: #7510f6;
+  text-align: center;
 }
 </style>
--- Portfolio-pro/src/views/AboutView.vue
+++ Portfolio-pro/src/views/AboutView.vue
@@ -1,15 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="profile">
+    <h1>Profil</h1>
+
+    <div class="big-section">
+      <div class="flex">
+        <section class="section">
+          <h2>Section 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.</p>
+        </section>
+
+        <section class="section">
+          <h2>Section 2</h2>
+          <p>Sed orci ipsum, malesuada nec pellentesque sit amet, vestibulum in velit.</p>
+        </section>
+
+        <section class="section">
+          <h2>Section 3</h2>
+          <p>Proin eu purus eget diam vehicula aliquet sit amet a lectus.</p>
+        </section>
+      </div>
+    </div>
+
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+
+.big-section {
+  border-radius: 20px 20px 0 0;
+  overflow: hidden;
 }
+
+.flex {
+  display: flex;
+  justify-content: space-between;
+  gap: 0; 
+}
+
+.section {
+  width: 33.33%; 
+  padding: 20px;
+  border: 0.2px solid #ccc;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
 </style>
